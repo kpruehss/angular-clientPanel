eca447e357e2773fc89e977f6278a633faf7a334
Karsten Pruehss <Karsten.Pruehss@gmail.com>
2018-09-14 12:47:34 +1000

    Implemented delete and edit logic + forms

--- src/app/components/client-details/client-details.component.ts
+++ src/app/components/client-details/client-details.component.ts
@@ -45,4 +45,15 @@ export class ClientDetailsComponent implements OnInit {
       timeout: 4000,
     });
   }
+
+  onDeleteClick() {
+    if (confirm('Are you sure?')) {
+      this.clientService.deleteClient(this.client);
+    }
+    this.flashMessage.show('Client Removed', {
+      cssClass: 'alert-success',
+      timeout: 4000,
+    });
+    this.router.navigate(['/']);
+  }
 }
