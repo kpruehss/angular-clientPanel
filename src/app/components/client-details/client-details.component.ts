30df22bee4924c65b0b0d8d3b58894863f0f4dc1
Karsten Pruehss <Karsten.Pruehss@gmail.com>
2018-09-14 12:25:52 +1000

    Added balance tracking and updating

--- src/app/components/client-details/client-details.component.ts
+++ src/app/components/client-details/client-details.component.ts
@@ -37,4 +37,12 @@ export class ClientDetailsComponent implements OnInit {
       this.client = client;
     });
   }
+
+  updateBalance() {
+    this.clientService.updateClient(this.client);
+    this.flashMessage.show('Balance updated', {
+      cssClass: 'alert-success',
+      timeout: 4000,
+    });
+  }
 }
